81d8eb0295e7dc402b78c496150f9894e609ea4c
bboomja <gilrieom@icloud.com>
2023-11-21 14:13:16 -0800

    Implement functions to fetch algebra and chemistry quizzes from backend

--- quizController.js
+++ quizController.js
@@ -1,1 +1,11 @@
 // This file would contain functions to handle the logic for each route, like getting quizzes, taking a quiz, etc.
+
+
+// Quiz retrieval
+// Implement a function to handle GET requests for fetching available quizzes.
+// This will involve querying our database and returning the quiz data.
+
+// Submit Answers 
+// Implement a fuction to handle POST requests for submitting quiz answers.
+// This function will process the answers, update the database if necessary, and return the result.
+
--- quiz_taker.js
+++ quiz_taker.js
@@ -1,2 +1,70 @@
 // This file would handle requests related to quizzes, such as fetching quiz questions, submitting answers, and tracking progress.
+// This file will be responsible for the frontend logic of our quiz taker functionality. It will interact with the user interface
+// and make requests to the backend.
 
+// Fetch Quizzes
+// Implement a function to fetch available quizzes from the backend.
+// This will likely make a GET request to an endpoint defined in index.js? (not sure yet)
+// Fetch algebra quizzes
+const fetchAlgebraQuizzes = async () => {
+    try {
+        const response = await fetch('http://localhost:3000/algebra_questions');
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const quizzes = await response.json();
+        return quizzes;
+    } catch (error) {
+        console.error('Error fetching algebra quizzes: ', error);
+    }
+};
+
+// Fetch Chemistry Quizzes
+const fetchChemistryQuizzes = async () => {
+    try {
+        const response = await fetch('http://localhost:3000/chemistry_questions');
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const quizzes = await response.json();
+        return quizzes;
+    } catch (error) {
+        console.error('Error fetching chemistry quizzes:', error);
+    }
+};
+
+// Example usage
+fetchAlgebraQuizzes().then(quizzes => {
+    if (quizzes) {
+        console.log('Algebra Quizzes:', quizzes);
+        // Display these quizzes on the page
+    }
+});
+
+fetchChemistryQuizzes().then(quizzes => {
+    if (quizzes) {
+        console.log('Chemistry Quizzes:', quizzes);
+        // Display these quizzes on the page
+    }
+});
+
+
+// Display Quizzes
+// Once fetch the quizzes, display them on the page.
+// User should be able to select a quiz to take.
+
+
+
+// Handle Quiz interaction
+// Implement fuctions to handle quiz interactions like displaying questions, selecting answers,
+// and navigating between questions.
+
+
+
+// Submit Answers
+// Implement a fuction to submit answers to the backend, probably a POST request to an endpoint
+
+
+
+// Track progress
+// Add features to track and display the user's progress through the quiz.
